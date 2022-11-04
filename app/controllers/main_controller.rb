570d49e70a4f310d391852899c06a16238532332
Saritha K S <saritha.ks@trivand.com>
2022-11-04 16:55:11 +0400

    alert and notification messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
-    def index        
+    def index    
+            flash.now[:notice] = "Some home page alert like successfull login"
+            flash.now[:alert] = "Some Alert Message"
+
     end
 end
